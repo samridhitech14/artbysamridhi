658a3297540373e2908967a7ea7ee45f0c72ddd3
samridhitech14 <samridhi.tech14@gmail.com>
2026-03-28 00:54:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const menuOpenButton = document.querySelector("#menu-open-button");
-
-menuOpenButton.addEventListener("click", () => {
-    document.body.classList.toggle("show-mobile-menu");
-});
 
